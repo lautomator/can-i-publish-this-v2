75c12ec809260cb906d6ec15309c9e6ca95b38c0
John Merigliano <jmerigliano@gmail.com>
2022-04-02 10:04:03 -0400

    putting some new rendered views in place

--- can_i_publish/libel/views.py
+++ can_i_publish/libel/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Card, Relationship
+
 
 def index(request):
-    return HttpResponse("This is home of Libel with Q1")
+    return HttpResponse("This is home of Libel with all of the modules.")
 
 
-def card(request, card_id):
-    return HttpResponse("This is a card view.")
+# def card(request, card_id):
+#     card = get_object_or_404(Card, pk=card_id)
+#     return render(request, 'libel/card')
+
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
