8c76842976d4e256d2d1c324cebb4be67d71232d
John Merigliano <john.merigliano@thefire.org>
2022-04-06 14:41:22 -0700

    added all data to card table and some temporary styles

--- can_i_publish/libel/models.py
+++ can_i_publish/libel/models.py
@@ -31,8 +31,8 @@ class Card(models.Model):
         null=True
     )
 
-    choice_one = models.CharField("Choice 1", max_length=32, blank=True, default="yes")
-    choice_two = models.CharField("Choice 2", max_length=32, blank=True, default="no")
+    choice_one = models.CharField("Choice 1", max_length=144, blank=True, default="yes")
+    choice_two = models.CharField("Choice 2", max_length=144, blank=True, default="no")
 
     def __str__(self):
         return self.card_slug
