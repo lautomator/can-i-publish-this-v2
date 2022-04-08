fe467876606068d8756db653c3d79d015be6e98b
John Merigliano <john.merigliano@thefire.org>
2022-04-08 14:57:22 -0700

    added init static files; working with python backend history function

---
+++ can_i_publish/libel/static/main.js
@@ -0,0 +1,39 @@
+var libel_main_script = {
+    init_question: "Q1",
+    targets: {
+        card_slug: document.querySelector("#card-slug"),
+    },
+    store_card_slug(slug) {
+        var history = localStorage.getItem("card_history");
+        // console.log(typeof(history), history);
+        history = history + ", " + slug;
+        localStorage.setItem("card_history", history);
+    },
+    reset: function(slug) {
+        localStorage.clear();
+        localStorage.setItem("card_history", slug);
+    },
+    init: function() {
+        // setup local storage for the card history (first time only)
+        if (localStorage.getItem("card_history") === null) {
+            this.reset(this.init_question);
+        }
+
+        // get the current card slug
+        if (this.targets.card_slug !== null) {
+            // first, check for a reset: the visitor started over
+            if (this.targets.card_slug.dataset.slug === this.init_question) {
+                this.reset(this.init_question)
+            } else {
+                // add the current card slug to the history
+                this.store_card_slug(this.targets.card_slug.dataset.slug);
+            }
+        }
+
+        console.log(localStorage);
+    }
+
+
+};
+
+libel_main_script.init();
--- can_i_publish/libel/views.py
+++ can_i_publish/libel/views.py
@@ -11,6 +11,21 @@ from .models import Card, Relationship
 CARDS = Card.objects.all()
 RELS = Relationship.objects.all()
 
+card_history = {
+    'path': ['Q1'],
+    'current_card': '',
+}
+
+def update_card_history(slug):
+    card_history['current_card'] = slug
+    card_history['path'].append(slug)
+
+
+def reset_card_history():
+    card_history['path'] = []
+    card_history['current_card'] = []
+
+
 def index(request):
     # index should go to Q1
     return HttpResponseRedirect(reverse('card', args=('Q1',)))
@@ -19,7 +34,19 @@ def index(request):
 def card(request, card_slug):
     card = CARDS.get(card_slug=card_slug)
     rel = RELS.get(card=card.id)
-    context = {'card': card, 'rel': rel}
+
+    # reset if we get to Q1 again
+    if card_slug == 'Q1':
+        # reset the history
+        reset_card_history()
+        update_card_history('Q1')
+
+    # avoid duplicate hits if the page is refreshed
+    # or if the user goes back to the earlier state.
+    if card_history['path'][-1] != card_slug and card_history['path'][-1] != rel.choice_back:
+        update_card_history(card_slug)
+
+    context = {'card': card, 'rel': rel, 'card_history': card_history}
 
     return render(request, 'libel/card.html', context)
 
