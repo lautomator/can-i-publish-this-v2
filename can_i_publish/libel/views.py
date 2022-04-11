1d6cf5c872b2d290b0f8f71921415994c140198b
John Merigliano <john.merigliano@thefire.org>
2022-04-11 10:57:54 -0700

    we have a working history system.

--- can_i_publish/libel/views.py
+++ can_i_publish/libel/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 
 from .models import Card, Relationship
 
-import can_i_functions
 
 # These are only updated by the admin.
 # They could be part of an API and they
@@ -15,9 +14,29 @@ RELS = Relationship.objects.all()
 
 card_history = {
     'path': ['Q1'],
-    'current_card': '',
+    'history': ['Q1'],
 }
 
+# ~~~~~~~~~~~~~~~~~~~
+# Functions
+# ~~~~~~~~~~~~~~~~~~~
+
+def update_card_history(slug):
+    card_history['path'].append(slug)
+
+    # do not include duplicates
+    if slug not in card_history['history']:
+        card_history['history'].append(slug)
+
+
+def reset_card_history():
+    card_history['path'] = []
+    card_history['history'] = []
+
+
+# ~~~~~~~~~~~~~~~~~~~
+# Views
+# ~~~~~~~~~~~~~~~~~~~
 
 def index(request):
     # index should go to Q1
@@ -29,15 +48,15 @@ def card(request, card_slug):
     rel = RELS.get(card=card.id)
 
     # reset if we get to Q1 again
-    # if card_slug == 'Q1':
-    #     # reset the history
-    #     reset_card_history()
-    #     update_card_history('Q1')
+    if card_slug == 'Q1':
+        # reset the history
+        reset_card_history()
+        update_card_history('Q1')
 
-    # # avoid duplicate hits if the page is refreshed
-    # # or if the user goes back to the earlier state.
-    # if card_history['path'][-1] != card_slug and card_history['path'][-1] != rel.choice_back:
-    #     update_card_history(card_slug)
+    # avoid duplicate hits if the page is refreshed
+    # or if the user goes back to the earlier state.
+    if card_history['path'][-1] != card_slug and card_history['path'][-1] != rel.choice_back:
+        update_card_history(card_slug)
 
     context = {'card': card, 'rel': rel, 'card_history': card_history}
 
@@ -45,6 +64,9 @@ def card(request, card_slug):
 
 
 def summary(request, last_card_slug):
-    return render(request, 'libel/summary.html', {'last_card': last_card_slug})
+    context = {
+        'last_card': last_card_slug,
+        'card_history': card_history,
+    }
+    return render(request, 'libel/summary.html', context)
 
-can_i_functions.test(1000009)
