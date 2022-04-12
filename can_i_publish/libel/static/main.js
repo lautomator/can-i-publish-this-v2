0c2102416ff7c0a3b90f036de36ed3ed971c4730
John Merigliano <john.merigliano@thefire.org>
2022-04-12 14:10:35 -0700

    working js for choice buttons

--- can_i_publish/libel/static/main.js
+++ can_i_publish/libel/static/main.js
@@ -1,17 +1,18 @@
 var libel_main_script = {
     targets: {
-        choices: document.querySelectorAll(".choice"),
+        choices: document.querySelectorAll(".choice")
     },
     button_click: function (e) {
         var choice = e.target.dataset.choice;
+        var slug = e.target.dataset.slug;
         var url_path = "/libel/" + choice;
-        // console.log(choice);
 
-        // if (choice === "None") {
-        //     url_path = "libel/summary/";
-        // }
+        if (choice === "None") {
+            url_path = "libel/" + slug + "/summary/";
+        }
 
         window.location.pathname = url_path;
+
     },
     init: function() {
         // listen for button clicks
@@ -24,8 +25,6 @@ var libel_main_script = {
                 this.targets.choices[index].onclick = this.button_click;
                 index += 1;
             }
-
-
         }
     }
 
