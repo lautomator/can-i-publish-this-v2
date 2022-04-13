9bb41d73517a2b011fa5204e2244f79b43b21424
John Merigliano <john.merigliano@thefire.org>
2022-04-13 15:04:34 -0700

    have working summary with question slugs and answers

--- can_i_publish/libel/views.py
+++ can_i_publish/libel/views.py
@@ -24,14 +24,71 @@ card_history = {
 def update_card_history(slug):
     card_history['path'].append(slug)
 
-    # do not include duplicates
-    if slug not in card_history['history']:
+    # do not include duplicates, continuation cards, and end cards
+    if slug not in card_history['history'] and slug[0] != 'A':
         card_history['history'].append(slug)
+
 
 
 def reset_card_history():
     card_history['path'] = []
     card_history['history'] = []
+
+
+def get_answer(slug, next_slug):
+    """
+    you can get the answer to the question by looking at
+    the previous slug choices (rels). You get that slug from
+    the history (the previous slug) and matching it with the
+    next slug in the history.
+    ex history: [Q1, Q2, E2]
+    Q1 choice = if rel.choice Q1 == Q2 then rel.choice = rel.choice 'yes'
+
+    >>> from libel.models import Card, Relationship
+    >>> CARDS = Card.objects.all()
+    >>> RELS = Relationship.objects.all()
+    >>> card = CARDS.get(card_slug='Q1')
+    >>> card
+    <Card: Q1>
+    >>> card.choice_one
+    'yes'
+    >>> rel = RELS.get(card=card.id)
+    >>> rel
+    <Relationship: Q1 rel>
+    >>> rel.choice_one_next
+    <Card: Q2>
+    >>> rel.choice_two_next
+    <Card: E1>
+    rel.choice_two_next.card_slug == 'E1'
+    True
+    >>> card.choice_two
+    'no'
+
+    Therefore, you answered 'no' to Question Q1.
+    """
+
+    card = CARDS.get(card_slug=slug)
+    rel = RELS.get(card=card.id)
+    answer_text = ''
+
+    if rel.choice_one_next.card_slug == next_slug:
+        answer_text = card.choice_one
+    else :
+        answer_text = card.choice_two
+
+    return answer_text
+
+
+def gen_answer_index():
+    answers = {}
+    history = card_history['history']
+    index = 0
+
+    while index < (len(history) - 1):
+        answers[history[index]] = get_answer(history[index], history[index + 1])
+        index += 1;
+
+    return answers
 
 
 # ~~~~~~~~~~~~~~~~~~~
@@ -70,6 +127,7 @@ def summary(request, last_card_slug):
     context = {
         'last_card': last_card_slug,
         'card_history': card_history,
+        'answers': gen_answer_index()
     }
     return render(request, 'libel/summary.html', context)
 
