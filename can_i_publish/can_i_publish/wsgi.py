75f4060b84a257457110f6daec733dfdcb5bff0d
John Merigliano <jmerigliano@gmail.com>
2022-04-29 11:21:11 -0700

    changed path to settings

--- can_i_publish/can_i_publish/wsgi.py
+++ can_i_publish/can_i_publish/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'can_i_publish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'can_i_publish.can_i_publish.settings')
 
 application = get_wsgi_application()
