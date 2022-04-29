53a30089c621eb42c583d219ee160022c4b81c9b
John Merigliano <jmerigliano@gmail.com>
2022-04-29 13:57:58 -0700

    added env var to settings

--- can_i_publish/settings.py
+++ can_i_publish/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,10 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-key_target = BASE_DIR / 'can_i_publish/secrets.txt'
-with open(key_target) as f:
-    SECRET_KEY = f.read().strip()
-
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
